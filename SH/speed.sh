398706dc3b25317fdb123b7e937208a89fee85a9
FGonzalez <felixsebasgc@users.noreply.github.com>
2023-03-12 10:18:47 -0500

    Update speed.sh

--- SH/speed.sh
+++ SH/speed.sh
@@ -1,11 +1,11 @@
 #!/system/bin/sh
 
 echo Please wait, it will take a few minutes ...
-echo STEP 1
+echo "STEP 1"
 cmd package compile -m speed-profile -f -a
-echo STEP 2
+echo "STEP 2"
 cmd package compile -a -f --compile-layouts
-echo STEP 3
+echo "STEP 3"
 cmd package bg-dexopt-job
 
 echo "Done"
